b361e228cbac05b1825dec2e8dad2d2e56dedfe1
leonel003 <leonel-flores-98@outlook.com>
2025-03-19 11:21:06 -0300

    Delete proyecto utn directory

--- proyecto utn/team.js
+++
@@ -1,6 +0,0 @@
-window.onload= ()=>{
-    let cardImage = document.querySelectorAll('.card-photo');
-    for(let x=0; x< cardImage.length;x++){
-        cardImage[x].setAttribute('src', `images/persona-${x}.jpg`);
-    }
-}
